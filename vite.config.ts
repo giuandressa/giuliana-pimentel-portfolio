81558543c672dc5749c35f90f9701b389e670cca
Giuliana Pimentel <giu.andressa@gmail.com>
2026-06-08 12:40:44 -0300

    force nitro deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@
 //  tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    server: { entry: "server" },
+    //server: { entry: "server" },
 //  },
 //});
 
